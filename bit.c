3f7d74080cbcfb977c7915d484c9a16fb61d24bb
Harsh Sandhu <64521805+harsh99sandhu@users.noreply.github.com>
2021-03-18 01:06:21 +0530

    Add files via upload

---
+++ bit.c
@@ -0,0 +1,56 @@
+/*
+Name: Harshpreet Singh Sandhu
+Roll no: 65
+*/
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+ int i=0,k=0,count=0;
+ char bits[80],debits[80];
+
+ printf("Enter Data Bits:\n");
+ scanf("%s",bits);
+ 
+ printf("Data Bits after Bit Stuffing:\n");
+  
+ for(i=0; i<strlen(bits); i++)
+ {
+    if(bits[i]=='1')
+        count++;
+    else
+        count=0;
+    printf("%c",bits[i]);
+    if(count==5)
+    {
+        printf("0");
+        count=0;
+    }
+ }
+ 
+ printf("\nEnter data Bits for Bit Destuffing:\n");
+ scanf("%s",debits);
+  count=0;
+ for(i=0;i<strlen(debits);i++)
+ {
+ 	if(debits[i]=='1')
+		count++;
+	else
+		count=0;
+	if(count==5)
+	{
+		i=i+1;
+		for(k=i;k<strlen(debits);k++)
+		debits[k]=debits[k+1];
+		debits[k]='\0';
+		i=i-1;
+	}
+ }
+ 
+ printf("Data Bits after Bit Destuffing:\n%s",debits);
+
+return 0;
+}
+
+
---
+++ hamming.c
@@ -0,0 +1,60 @@
+/* 
+	Harshpreet Singh Sandhu
+    65
+*/ 
+#include<stdio.h>
+ 
+void main() 
+{
+    int data[10],datarec[10],c,c1,c2,c3,i;
+ 
+    printf("Enter 4 bits of data one by one (using even parity)\n");
+	scanf("%d",&data[0]);
+    scanf("%d",&data[1]);
+    scanf("%d",&data[2]);
+    scanf("%d",&data[4]);
+ 
+    //Calculation of even parity
+    data[6]=data[0]^data[2]^data[4];
+	data[5]=data[0]^data[1]^data[4];
+	data[3]=data[0]^data[1]^data[2];
+ 
+	printf("\nEncoded data is\n");
+	for(i=0;i<7;i++)
+        printf("%d",data[i]);
+ 
+    printf("\n\nEnter received data bits one by one\n");
+    for(i=0;i<7;i++)
+        scanf("%d",&datarec[i]);
+ 
+    c1=datarec[6]^datarec[4]^datarec[2]^datarec[0];
+	c2=datarec[5]^datarec[4]^datarec[1]^datarec[0];
+	c3=datarec[3]^datarec[2]^datarec[1]^datarec[0];
+	c=c3*4+c2*2+c1 ;
+ 
+    if(c==0) {
+		printf("\nNo error while transmission of data\n");
+    }
+	else {
+		printf("\nError on position %d",c);
+    	
+		printf("\nData sent : ");
+        for(i=0;i<7;i++)
+        	printf("%d",data[i]);
+        
+		printf("\nData received : ");
+        for(i=0;i<7;i++)
+        	printf("%d",datarec[i]);
+		
+		printf("\nCorrect message is\n");
+ 
+		if(datarec[7-c]==0)
+			datarec[7-c]=1;
+        else
+			datarec[7-c]=0;
+		
+		for (i=0;i<7;i++) {
+			printf("%d",datarec[i]);
+		}
+	}
+}
